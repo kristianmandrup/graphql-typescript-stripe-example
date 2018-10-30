56d160f72d64ae9487177695d7d13332d407c543
Kristian Mandrup <kmandrup@gmail.com>
2018-10-30 14:25:43 +0000

    fix logout mutation

--- web/src/modules/user/logout/mutation.ts
+++ web/src/modules/user/logout/mutation.ts
@@ -2,7 +2,7 @@ import { gql } from "apollo-boost";
 // import { userFragment } from "../../../graphql/fragments/userFragment";
 
 export const logoutMutation = gql`
-  mutation LogoutMutation() {
+  mutation {
     logout
   }
 `;
